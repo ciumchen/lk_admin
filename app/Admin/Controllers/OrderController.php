b8cad2c2e85d40bdd9a70b9fb06330a6e189ebf9
ning <ning@qq.com>
2021-04-28 15:55:35 +0800

    更新app文件

--- app/Admin/Controllers/OrderController.php
+++ app/Admin/Controllers/OrderController.php
@@ -3,6 +3,7 @@
 namespace App\Admin\Controllers;
 
 use App\Admin\Actions\Grid\VerifyOrder;
+use App\Admin\Repositories\LkPhoneBillOrder;
 use App\Admin\Repositories\Order;
 use Dcat\Admin\Form;
 use Dcat\Admin\Grid;
@@ -18,22 +19,29 @@ class OrderController extends AdminController
      */
     protected function grid()
     {
-        return Grid::make(new Order(), function (Grid $grid) {
+        return Grid::make(new LkPhoneBillOrder(), function (Grid $grid) {
             $grid->model()->orderBy('id','desc');
-            $grid->model()->with(['user','business']);
+            $grid->model()->where('description','=',"LR");
+            $grid->model()->with(['user']);
             $grid->column('id')->sortable();
-            $grid->column('uid');
 
-            $grid->column('business_uid');
-            $grid->column('business.phone',"商家手机号");
-            $grid->column('user.phone','消费者手机号');
-            $grid->column('profit_ratio');
+//            $grid->column('goods_id');
+            $grid->column('order_no');//订单号
+//            $grid->column('goods_bn');
+            $grid->column('title');
             $grid->column('price');
-            $grid->column('profit_price');
-            $grid->column('status')->display(function ($v){
-                return Order::$statusLabel[$v] ?? $v;
-            });
-            $grid->column('pay_status')->display(function($v){
+            $grid->column('num');
+//            $grid->column('shop_id');//所属商家
+            $grid->column('profit_ratio')->display(function () {
+                return ($this->profit_ratio*100).'%';
+            });//让利比例
+            $grid->column('integral');//获得积分
+
+            $grid->column('user.username','买家用户名');//买家用户名
+            $grid->column('numeric','充值手机号');//充值手机号
+            $grid->column('telecom','运营商');//运营商
+
+            $grid->column('status')->display(function($v){
                 if($v=='await'){
                     return '待支付';
                 }elseif($v=='pending'){
@@ -45,108 +53,92 @@ class OrderController extends AdminController
                 }else{
                     return "订单异常";
                 }
-            });//支付状态
-//            支付状态：await 待支付；pending 支付处理中； succeeded 支付成功；failed 支付失败
+            });//订单状态
 
-            $grid->column('name');
-            $grid->column('remark');
-            $grid->column('created_at');
-            $grid->column('updated_at')->sortable();
+            $grid->column('refund_fee');
+            $grid->column('aftersales_status');
+            $grid->column('order_from');
 
-            // 禁用创建按钮
-            $grid->disableCreateButton();
-            // 禁用编辑按钮
-            $grid->disableEditButton();
+            $grid->column('timeout_action_time');
+            $grid->column('pay_time')->display(function (){
+                return date('Y-m-d H:i:s',$this->pay_time);
+            });
+            $grid->column('end_time');
+            $grid->column('modified_time');
+
             // 禁用删除按钮
             $grid->disableDeleteButton();
-            // 禁用显示按钮
+            //禁用新增按钮
+            $grid->disableCreateButton();
+            //禁用显示按钮
             $grid->disableViewButton();
-
-
-            //筛选
-            $grid->filter(function (Grid\Filter $filter) {
-                $filter->equal('id');
-                $filter->equal('uid');
-                $filter->equal('business_uid');
-                $filter->equal('status')->select(function () {
-                    return Order::$statusLabel;
-                });
-                //支付状态
-                $filter->equal('pay_status')->select(function () {
-                    return Order::$pay_status;
-                });
-
-            });
-
-            //审核订单按钮
-            $grid->actions(function (Grid\Displayers\Actions $actions) {
-                if($actions->row->status == Order::STATUS_DEFAULT)
-                {
-                    $actions->append(new VerifyOrder());
-                }
-            });
-
-
-
+            //禁用编辑按钮
+            $grid->disableEditButton();
 
             $titles = [
                 'id' => 'ID',
-                'uid' => '消费者UID',
-                'business_uid' => '	商家UID',
-                'business.phone' => '商家手机号',
-                'profit_ratio' => '让利比列(%)',
-                'user.phone' => '消费者手机号',
-                'price' => '消费金额',
-                'profit_price' => '实际让利金额',
-                'status' => '审核状态',
-                'pay_status' => '	支付状态',
-                'name' => '消费商品名',
-                'remark' => '备注',
-                'created_at' => '创建时间',
-                'updated_at' => '更新时间',
+//                'goods_id' => '商品ID',
+                'order_no' => '订单号',
+//                'goods_bn' => '明细商品的编码',
+                'title' => '商品标题',
+
+                'num' => '购买数量',
+//                'shop_id' => '所属商家',
+                'profit_ratio' => '商家让利',
+                'integral' => '获得积分',
+                'user.username' => '买家用户名',
+
+                'numeric' => '充值手机号',
+                'telecom' => '运营商',
+                'price' => '商品价格',
+
+                'status' => '订单支付状态',
+                'refund_fee' => '退款金额',
+                'aftersales_status' => '售后状态',
+                'order_from' => '订单来源',
+                'timeout_action_time' => '订单超时到期时间',
+                'pay_time' => '付款时间',
+                'end_time' => '结束时间',
+                'modified_time' => '最后更新时间',
 
             ];
             $grid->export($titles)->rows(function (array $rows) {
                 foreach ($rows as $index => &$row) {
                     // 这里假设role就是关联数据
-                    $row['user.phone'] = $row['user']['phone'];
-                    $row['business.phone'] = $row['business']['phone'];
-
-                    if($row['status']==1){
-                        $row['status']="审核中";
-                    }elseif($row['status']==2){
-                        $row['status']="审核通过";
-                    }elseif($row['status']==3){
-                        $row['status']="审核失败";
-                    }
-
-                    //支付状态
-                    if($row['pay_status']=='await'){
-                        $row['pay_status']="待支付";
-                    }elseif($row['pay_status']=='pending'){
-                        $row['pay_status']="支付处理中";
-                    }elseif($row['pay_status']=='succeeded'){
-                        $row['pay_status']="支付成功";
-                    }elseif($row['pay_status']=='failed'){
-                        $row['pay_status']="支付失败";
+                    $row['user.username'] = $row['user']['username'];
+//                    $row['user.phone'] = $row['numeric'];
+                    if($row['status']=='await'){
+                        $row['status']="待支付";
+                    }elseif($row['status']=='pending'){
+                        $row['status']="支付处理中";
+                    }elseif($row['status']=='succeeded'){
+                        $row['status']="支付成功";
+                    }elseif($row['status']=='failed'){
+                        $row['status']="支付失败";
                     }else{
-                        $row['pay_status']="订单异常";
+                        $row['status']="订单异常";
                     }
+
+                    $row['pay_time']=date('Y-m-d H:i:s',$row['pay_time']);
 
                 }
                 return $rows;
             });
 
+            //筛选
+            $grid->filter(function (Grid\Filter $filter) {
+                $filter->equal('id');
+                $filter->equal('goods_id');
+                $filter->equal('shop_id');
 
+                $filter->equal('numeric','充值手机号');
 
-
-
-
-
-
-
-
-
+                $filter->equal('order_no');
+                //支付状态
+                $filter->equal('status')->select(function () {
+                    return Order::$pay_status;
+                });
+            });
         });
     }
 
@@ -157,43 +149,32 @@ class OrderController extends AdminController
      *
      * @return Show
      */
-    protected function detail($id)
-    {
-        return Show::make($id, new Order(), function (Show $show) {
-            $show->field('id');
-            $show->field('uid');
-            $show->field('business_uid');
-            $show->field('profit_ratio');
-            $show->field('price');
-            $show->field('profit_price');
-            $show->field('status');
-            $show->field('name');
-            $show->field('remark');
-            $show->field('created_at');
-            $show->field('updated_at');
-        });
-    }
+//    protected function detail($id)
+//    {
+//        return Show::make($id, new LkPhoneBillOrder(), function (Show $show) {
+//            $show->field('id');
+//            $show->field('shop_id');
+//            $show->field('user_id');
+//            $show->field('goods_id');
+//            $show->field('created_at');
+//            $show->field('updated_at');
+//        });
+//    }
 
     /**
      * Make a form builder.
-     *
+     *编辑订单
      * @return Form
      */
-    protected function form()
-    {
-        return Form::make(new Order(), function (Form $form) {
-            $form->display('id');
-            $form->text('uid');
-            $form->text('business_uid');
-            $form->text('profit_ratio');
-            $form->text('price');
-            $form->text('profit_price');
-            $form->text('status');
-            $form->text('name');
-            $form->text('remark');
-
-            $form->display('created_at');
-            $form->display('updated_at');
-        });
-    }
+//    protected function form()
+//    {
+//        return Form::make(new LkPhoneBillOrder(), function (Form $form) {
+////            $form->display('id');
+////            $form->display('shop_id');
+////            $form->display('user_id');
+////            $form->display('order_no');
+//            $form->text('status');
+//
+//        });
+//    }
 }

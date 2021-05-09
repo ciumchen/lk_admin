3445eaa5e2d5b8043fc0e5421ff694d5c14d81fe
ning <ning@qq.com>
2021-05-09 12:04:10 +0800

    更新商家申请数据库迁移文件

--- database/migrations/2021_05_08_171512_add_business_apply2_table.php
+++ database/migrations/2021_05_08_171512_add_business_apply2_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateBusinessApplyTable extends Migration
+class AddBusinessApply2Table extends Migration
 {
     /**
      * Run the migrations.
@@ -14,10 +14,10 @@ class UpdateBusinessApplyTable extends Migration
     public function up()
     {
         Schema::table('business_apply',function (Blueprint $table){
-            $table->string('img_just',255)->nullable()->comment('身份证正面照')->change();
-            $table->string('img_back',255)->nullable()->comment('身份证反面照')->change();
-            $table->string('img_hold',255)->nullable()->comment('身份证手持照')->change();
-            $table->string('img_details',255)->nullable()->comment('身份证详情照')->change();
+            $table->string('img_just',255)->comment('身份证正面照');
+            $table->string('img_back',255)->comment('身份证反面照');
+            $table->string('img_hold',255)->comment('身份证手持照');
+            $table->string('img_details',255)->comment('身份证详情照');
         });
     }
 

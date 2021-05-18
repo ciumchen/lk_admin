addcce14d979c14c7d51b6d63fdd4e4d846f881b
ning <ning@qq.com>
2021-05-18 11:09:42 +0800

    不格式日期

--- app/Admin/Controllers/BusinessDataController.php
+++ app/Admin/Controllers/BusinessDataController.php
@@ -124,17 +124,14 @@ class BusinessDataController extends AdminController
 
 //            $form->display('business_apply.img','营业执照');
 
-            $form->image('business_apply.img','营业执照')->uniqueName()->accept('jpg,png,gif,jpeg', 'image/*')->disk('oss')->move('/business/category')->autoUpload();
-
-
-
-            $form->display('business_apply.img2','商家门头照');
-            $form->display('business_apply.img_just','身份证正面');
-            $form->display('business_apply.img_back','身份证反面');
-            $form->display('business_apply.img_hold','身份证手持');
-            $form->display('business_apply.img_details1','商家详情照1');
-            $form->display('business_apply.img_details2','商家详情照2');
-            $form->display('business_apply.img_details3','商家详情照3');
+            $form->width(8)->image('business_apply.img','营业执照')->uniqueName()->accept('jpg,png,gif,jpeg', 'image/*')->disk('oss')->move('/business/category')->autoUpload();
+            $form->image('business_apply.img2','商家门头照')->uniqueName()->accept('jpg,png,gif,jpeg', 'image/*')->disk('oss')->move('/business/category')->autoUpload();
+            $form->image('business_apply.img_just','身份证正面')->uniqueName()->accept('jpg,png,gif,jpeg', 'image/*')->disk('oss')->move('/business/category')->autoUpload();
+            $form->image('business_apply.img_back','身份证反面')->uniqueName()->accept('jpg,png,gif,jpeg', 'image/*')->disk('oss')->move('/business/category')->autoUpload();
+            $form->image('business_apply.img_hold','身份证手持')->uniqueName()->accept('jpg,png,gif,jpeg', 'image/*')->disk('oss')->move('/business/category')->autoUpload();
+            $form->image('business_apply.img_details1','商家详情照1')->uniqueName()->accept('jpg,png,gif,jpeg', 'image/*')->disk('oss')->move('/business/category')->autoUpload();
+            $form->image('business_apply.img_details2','商家详情照2')->uniqueName()->accept('jpg,png,gif,jpeg', 'image/*')->disk('oss')->move('/business/category')->autoUpload();
+            $form->image('business_apply.img_details3','商家详情照3')->uniqueName()->accept('jpg,png,gif,jpeg', 'image/*')->disk('oss')->move('/business/category')->autoUpload();
 
             $form->display('created_at');
             $form->display('updated_at');

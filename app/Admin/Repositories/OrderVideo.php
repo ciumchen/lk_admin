0096771aefa74b9d7e4f184e2f464b84df536f6c
laydown <947714443@qq.com>
2021-06-25 18:02:26 +0800

    modify video order

--- app/Admin/Repositories/OrderVideo.php
+++ app/Admin/Repositories/OrderVideo.php
@@ -12,7 +12,7 @@ class OrderVideo extends EloquentRepository
     
     const CHANNEL_WANWEI      = 'ww'; /* 万维易源渠道标识 */
     const CHANNEL_BANMALIFANG = 'bm'; /* 斑马力方渠道标识 */
-    /* 订单类型状态 */
+    /* 订单类型 */
     const CREATE_TYPE_YOUKU       = 1; /* 优酷会员 */
     const CREATE_TYPE_XUNLEI      = 2; /* 迅雷会员 */
     const CREATE_TYPE_TUDOU       = 3; /* 土豆会员 */
@@ -23,6 +23,11 @@ class OrderVideo extends EloquentRepository
     const CREATE_TYPE_MONGO_TV    = 8; /* 芒果TV全屏会员 */
     const CREATE_TYPE_SOHU        = 9; /* 搜狐会员 */
     const CREATE_TYPE_TENCENT     = 10; /* 腾讯会员 */
+    /* 订单状态[第三方] */
+    const STATUS_DEFAULT = '0'; /* 默认状态[未获取] */
+    const STATUS_SUCCESS = '1'; /* 获取成功 */
+    const STATUS_FAIL    = '2'; /* 获取异常 */
+    const STATUS_CANCEL  = '9'; /* 撤销 */
     /**
      * @var string[] 渠道标识对应文字
      */
@@ -45,5 +50,15 @@ class OrderVideo extends EloquentRepository
         self::CREATE_TYPE_MONGO_TV    => '芒果TV全屏会员',
         self::CREATE_TYPE_SOHU        => '搜狐会员',
         self::CREATE_TYPE_TENCENT     => '腾讯会员',
+    ];
+    
+    /**
+     * @var string[] 订单状态对应文字
+     */
+    public static $statusTexts = [
+        self::STATUS_DEFAULT => '未获取',
+        self::STATUS_SUCCESS => '获取成功',
+        self::STATUS_FAIL    => '获取异常',
+        self::STATUS_CANCEL  => '撤销',
     ];
 }

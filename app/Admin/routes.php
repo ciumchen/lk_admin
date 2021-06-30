1ead9e1214bb6605bc2c60749c7a0bb8fff5927d
laydown <947714443@qq.com>
2021-06-30 16:04:20 +0800

    add affiche

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -6,69 +6,75 @@ use Illuminate\Support\Facades\Route;
 use Dcat\Admin\Admin;
 
 Admin::routes();
-Route::group([
-    'prefix'     => config('admin.route.prefix'),
-    'namespace'  => config('admin.route.namespace'),
-    'middleware' => config('admin.route.middleware'),
-], function (Router $router) {
-    //test测试
-    $router->resource('test', 'TestController');
-    $router->any('getShopUserInfo', 'TestController@getShopUserInfo');
-    $router->any('mytest', 'TestController@mytest');
-    $router->any('update_sjtt', 'TestController@update_sjtt');
-    $router->any('update_order_ddyc', 'TestController@update_order_ddyc');
-    $router->any('uploadImg', 'TestController@uploadImg');
-    $router->any('updateImg', 'TestController@updateImg');
-    $router->any('delApply', 'TestController@delApply');
-    $router->any('updateUser', 'TestController@updateUser');
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////
-    //话费订单、油卡订单、美团订单
-    $router->resource('hfdd', 'LkPhoneBillOrderController');
-    $router->resource('ykdd', 'LkOilCardOrderController');
-    $router->resource('mtdd', 'LkMeiTuanOrderController');
-    $router->resource('hfdc', 'LkPhoneDcController');
-    $router->resource('lkscsh', 'LkShopMerchantOrderController');
-    $router->resource('lksczy', 'LkShopSelfSupportOrderController');
-
-    $router->resource('sphy', 'VideoOrderController'); //视频会员订单
-    $router->resource('addjf', 'ToBeAddedIntegralController');
-    $router->resource('drddtj', 'DailyImportOrderStatisticController');//导入订单统计
-    $router->get('/', 'HomeController@index');
-    $router->resource('users', 'UserController');
-    $router->resource('user-data', 'UserDataController');
-    $router->resource('integral-logs', 'IntegralLogController');
-    $router->resource('business-category', 'BusinessCategoryController');
-    $router->resource('business-apply', 'BusinessApplyController');
-    $router->resource('business-data', 'BusinessDataController');
-    $router->resource('orders', 'OrderController');
-    $router->resource('assets-type', 'AssetsTypeController');
-    $router->resource('user-assets', 'AssetController');
-    $router->resource('assets-logs', 'AssetsLogController');
-    $router->resource('freeze-logs', 'FreezeLogController');
-    $router->resource('settings', 'SettingController');
-    $router->resource('ad', 'AdController');
-    $router->resource('city-data', 'CityDataController');
-    $router->resource('address', 'AddressController');
-    $router->resource('withdraw-logs', 'WithdrawLogController');
-    $router->resource('rebate-data', 'RebateDataController');
-    $router->resource('city-node', 'CityNodeController');
-    //上传图标
-    $router->any('file/upload-img', 'FileController@handle');
-    //系统消息
-    $router->resource('add-msg', 'MessageController');
-    $router->resource('save', 'MessageController');
-    //自营消息
-    $router->resource('self-msg', 'SelfMessageController');
-    $router->resource('self-save', 'SelfMessageController');
-    //机票退订
-    $router->resource('air-refund', 'AirRefundController');
-    $router->resource('air-send', 'AirRefundController');
-});
-Route::group([
-    'prefix'    => config('admin.route.prefix'),
-    'namespace' => config('admin.route.namespace'),
-], function (Router $router) {
-    //获取城市
-    $router->get('get-city', 'CityNodeController@getCity');
-});
+Route::group(
+    [
+        'prefix'     => config('admin.route.prefix'),
+        'namespace'  => config('admin.route.namespace'),
+        'middleware' => config('admin.route.middleware'),
+    ],
+    function (Router $router) {
+        //test测试
+        $router->resource('test', 'TestController');
+        $router->any('getShopUserInfo', 'TestController@getShopUserInfo');
+        $router->any('mytest', 'TestController@mytest');
+        $router->any('update_sjtt', 'TestController@update_sjtt');
+        $router->any('update_order_ddyc', 'TestController@update_order_ddyc');
+        $router->any('uploadImg', 'TestController@uploadImg');
+        $router->any('updateImg', 'TestController@updateImg');
+        $router->any('delApply', 'TestController@delApply');
+        $router->any('updateUser', 'TestController@updateUser');
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////
+        //话费订单、油卡订单、美团订单
+        $router->resource('hfdd', 'LkPhoneBillOrderController');
+        $router->resource('ykdd', 'LkOilCardOrderController');
+        $router->resource('mtdd', 'LkMeiTuanOrderController');
+        $router->resource('hfdc', 'LkPhoneDcController');
+        $router->resource('lkscsh', 'LkShopMerchantOrderController');
+        $router->resource('lksczy', 'LkShopSelfSupportOrderController');
+        $router->resource('sphy', 'VideoOrderController'); //视频会员订单
+        $router->resource('addjf', 'ToBeAddedIntegralController');
+        $router->resource('drddtj', 'DailyImportOrderStatisticController');//导入订单统计
+        $router->get('/', 'HomeController@index');
+        $router->resource('users', 'UserController');
+        $router->resource('user-data', 'UserDataController');
+        $router->resource('integral-logs', 'IntegralLogController');
+        $router->resource('business-category', 'BusinessCategoryController');
+        $router->resource('business-apply', 'BusinessApplyController');
+        $router->resource('business-data', 'BusinessDataController');
+        $router->resource('orders', 'OrderController');
+        $router->resource('assets-type', 'AssetsTypeController');
+        $router->resource('user-assets', 'AssetController');
+        $router->resource('assets-logs', 'AssetsLogController');
+        $router->resource('freeze-logs', 'FreezeLogController');
+        $router->resource('settings', 'SettingController');
+        $router->resource('ad', 'AdController');
+        $router->resource('city-data', 'CityDataController');
+        $router->resource('address', 'AddressController');
+        $router->resource('withdraw-logs', 'WithdrawLogController');
+        $router->resource('rebate-data', 'RebateDataController');
+        $router->resource('city-node', 'CityNodeController');
+        //上传图标
+        $router->any('file/upload-img', 'FileController@handle');
+        //系统消息
+        $router->resource('add-msg', 'MessageController');
+        $router->resource('save', 'MessageController');
+        //自营消息
+        $router->resource('self-msg', 'SelfMessageController');
+        $router->resource('self-save', 'SelfMessageController');
+        //机票退订
+        $router->resource('air-refund', 'AirRefundController');
+        $router->resource('air-send', 'AirRefundController');
+        $router->resource('aff-list', 'AfficheController');
+    }
+);
+Route::group(
+    [
+        'prefix'    => config('admin.route.prefix'),
+        'namespace' => config('admin.route.namespace'),
+    ],
+    function (Router $router) {
+        //获取城市
+        $router->get('get-city', 'CityNodeController@getCity');
+    }
+);
 
---
+++ resources/lang/zh-CN/affiche.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'labels'  => [
+        'Affiche'  => '公告',
+        'aff-list' => '公告列表',
+    ],
+    'fields'  => [
+        'id'      => 'ID',
+        'title'   => '标题',
+        'content' => '内容',
+    ],
+    'options' => [
+    ],
+];

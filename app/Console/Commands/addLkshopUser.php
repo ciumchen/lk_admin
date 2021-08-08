a31241b1a5257c866566cec3916a03b6a2f2dcb5
ning <ning@qq.com>
2021-08-08 12:34:23 +0800

    开启打印导入商城订单和用户日志信息

--- app/Console/Commands/addLkshopUser.php
+++ app/Console/Commands/addLkshopUser.php
@@ -46,7 +46,7 @@ class addLkshopUser extends Command
      */
     public function handle()
     {
-//        log::info('=================导入商城用户任务===================================');
+        log::info('========addLkshopUser=========导入商城用户任务===================================');
         //查询记录
         $LkUserModel = new LkshopAddUserLog();
         $addLog = $LkUserModel::where('type','addLkShopuser')->first();

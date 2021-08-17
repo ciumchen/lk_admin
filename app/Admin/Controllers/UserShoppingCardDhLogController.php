ba08e9c3808fba69ed26038c4489609a8d97cd0f
ning <ning@qq.com>
2021-08-17 16:58:52 +0800

    添加购物卡兑换记录列表2

---
+++ app/Admin/Controllers/UserShoppingCardDhLogController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Admin\Repositories\UserShoppingCardDhLog;
+use App\Models\UserLpjLog as UserLpjLogModel;
+use Dcat\Admin\Form;
+use Dcat\Admin\Grid;
+use Dcat\Admin\Show;
+use Dcat\Admin\Controllers\AdminController;
+
+class UserShoppingCardDhLogController extends AdminController
+{
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        return Grid::make(new UserShoppingCardDhLog(), function (Grid $grid) {
+            $grid->model()->orderBy('id','desc');
+            $grid->column('id')->sortable();
+            $grid->column('uid');
+            $grid->column('operate_type')->display(function ($v) {
+                if ($v=='exchange_dc'){
+                    return "代充";
+                } elseif ($v=='exchange_pl') {
+                    return "批量代充";
+                }elseif ($v=='exchange_mt') {
+                    return "美团";
+                }else{
+                    return "未知操作";
+                }
+            });
+            $grid->column('money');
+            $grid->column('money_before_change');
+            $grid->column('order_no');
+            $grid->column('status')->using(UserLpjLogModel::$statusLabel)->label(UserLpjLogModel::$statusLabelStyle);
+            $grid->column('created_at');
+            $grid->column('updated_at')->sortable();
+
+            $grid->disableCreateButton();
+            $grid->disableActions();
+            $grid->disableBatchDelete();
+
+            $grid->filter(function (Grid\Filter $filter) {
+                $filter->equal('id');
+                $filter->equal('uid');
+                $filter->equal('operate_type')->select(function () {
+                    return UserLpjLogModel::$operateTypeTexts;
+                });
+
+                $filter->equal('order_no');
+                $filter->equal('status')->select(function () {
+                    return UserLpjLogModel::$operateStatus;
+                });
+
+            });
+        });
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     *
+     * @return Show
+     */
+//    protected function detail($id)
+//    {
+//        return Show::make($id, new UserLpjLog(), function (Show $show) {
+//            $show->field('id');
+//            $show->field('created_at');
+//            $show->field('updated_at');
+//        });
+//    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+//    protected function form()
+//    {
+//        return Form::make(new UserLpjLog(), function (Form $form) {
+//            $form->display('id');
+//
+//            $form->display('created_at');
+//            $form->display('updated_at');
+//        });
+//    }
+}
--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -83,6 +83,10 @@ Route::group(
         $router->resource('withdraw-cash', 'WithdrawCashLogController');
         //来拼金记录
         $router->resource('user_lpj_log', 'UserLpjLogController');
+
+        //购物卡兑换记录列表
+        $router->resource('user_gwk_log', 'UserShoppingCardDhLogController');
+        
         /*********************** 拼团管理 ***********************/
         //新增拼团
         $router->resource('add-gather', 'GatherController');
@@ -90,6 +94,9 @@ Route::group(
         $router->resource('gather-trade', 'GatherTradeController');
         //用户拼团列表
         $router->resource('gather-winning', 'GatherWinningController');
+
+
+
     }
 );
 Route::group(

5294ed15f446aade5292188f0cad28dd9355523b
ning <ning@qq.com>
2021-08-26 15:02:04 +0800

    代充列表添加导出功能

--- app/Admin/Controllers/MobileAgentController.php
+++ app/Admin/Controllers/MobileAgentController.php
@@ -25,8 +25,8 @@ class MobileAgentController extends AdminController
         $thisController = $this;
         return Grid::make(new OrderMobile(), function (Grid $grid) use ($thisController) {
             $grid->model()->with(['orders','tradeOrder'])->orderByDesc('order_id');
+
             $grid->column('order_id')->sortable();
-            $grid->column('order_id');
             $grid->column('order_no');
             $grid->column('money');
             $grid->column('orders.profit_ratio', '商家让利')->display(function () {
@@ -60,13 +60,71 @@ class MobileAgentController extends AdminController
                 $filter->equal('uid');
                 $filter->equal('mobile');
                 $filter->equal('order_no');
-                $filter->equal('orders.pay_status', '支付状态');
+                $filter->equal('orders.pay_status', '支付状态')->select(function () {
+                    return Order::$pay_status;
+                });
 
             });
             $thisController->disableButtons($grid);
 
             $grid->actions(function (Grid\Displayers\Actions $actions) {
                 $actions->append(new MobileAgent());
+            });
+
+
+            $titles = [
+                'order_id' => '录单ID',
+                'order_no' => '订单号',
+                'money' => '充值金额',
+                'orders.profit_ratio' => '商家让利',
+                'uid' => '消费者UID',
+                'mobile' => '充值手机号',
+                'tradeOrder.order_from' => '订单来源',
+                'orders.pay_status' => '支付状态',
+                'status' => '充值状态',
+                'created_at' => '创建时间',
+                'updated_at' => '更新时间',
+
+            ];
+            $grid->export($titles)->rows(function (array $rows) {
+                foreach ($rows as $index => &$row) {
+                    // 这里假设role就是关联数据
+                    $row['orders.profit_ratio'] = $row['orders']['profit_ratio'];
+//                    $row['user.phone'] = $row['numeric'];
+                    if($row['status']==0){
+                        $row['status']="充值中";
+                    }elseif($row['status']==1){
+                        $row['status']="成功";
+                    }elseif($row['status']==9){
+                        $row['status']="撤销";
+                    }else{
+                        $row['status']="充值异常";
+                    }
+
+                    if($row['orders']['pay_status']=='await'){
+                        $row['orders.pay_status']="待支付";
+                    }elseif($row['orders']['pay_status']=='pending'){
+                        $row['orders.pay_status']="支付处理中";
+                    }elseif($row['orders']['pay_status']=='succeeded'){
+                        $row['orders.pay_status']="支付成功";
+                    }elseif($row['orders']['pay_status']=='failed'){
+                        $row['orders.pay_status']="支付失败";
+                    }else{
+                        $row['orders.pay_status']="订单异常";
+                    }//支付状态
+
+                    if($row['trade_order']['order_from']=='alipay'){
+                        $row['tradeOrder.order_from']="支付宝支付";
+                    }elseif($row['trade_order']['order_from']=='wx'){
+                        $row['tradeOrder.order_from']="微信支付";
+                    }elseif($row['trade_order']['order_from']=='gwk'){
+                        $row['tradeOrder.order_from']="购物卡";
+                    }else{
+                        $row['tradeOrder.order_from']="其他支付";
+                    }//订单来源
+
+                }
+                return $rows;
             });
 
         });

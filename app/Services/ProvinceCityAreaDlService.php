d19c9d23f3425c2cf6d1e2d80372b9a3ac5e95ab
ning <ning@qq.com>
2021-09-06 15:55:32 +0800

    修改省市区代理2

--- app/Services/ProvinceCityAreaDlService.php
+++ app/Services/ProvinceCityAreaDlService.php
@@ -16,30 +16,8 @@ use App\Models\UserCityData;
  */
 class ProvinceCityAreaDlService
 {
-    //分享佣金
-    public function inviteEncourage(Order $order, User $user, AssetsType $assets, $orderNo, $platformUid = 0)
-    {
-        if (intval($platformUid) == 0) {
-            $platformUid = Setting::getSetting('platform_uid');
-        }
-        try {
-            // 直属上级
-            $invite_first = $this->inviteEncourageFirst($order, $user, $assets, $orderNo, $platformUid);
-            $invite_second = $this->inviteEncourageSecond($order, $invite_first, $assets, $orderNo, $platformUid);
-            $this->inviteEncourageThird($order, $invite_second, $assets, $orderNo, $platformUid);
-        } catch (\Exception $e) {
-            throw $e;
-        }
-        return true;
-    }
-
     /*
      * 省市区代理返佣
-     * $order 订单记录对象
-     * $user 消费者user表记录对象
-     * $assets 获取资产类型记录对象
-     * $orderNo 订单号
-     * $platformUid 	来客平台账户UID=2
      */
     public function inviteProvinceCityAreaD(Order $order, User $user, AssetsType $assets, $orderNo, $platformUid = 0)
     {
@@ -82,9 +60,6 @@ class ProvinceCityAreaDlService
                 $districtAgentUid = $districtInfo->uid;
             }
 
-//            dd($provinceAgentUid,$cityAgentUid,$districtAgentUid);
-
-
         }
 
         //省代理返佣
@@ -93,14 +68,14 @@ class ProvinceCityAreaDlService
         if ($provinceNodeRebate > 0) {
             $remark = '省节点运营返佣';
             $provinceAmount = bcmul($order->profit_price, bcdiv($provinceNodeRebate, 100, 6), 8);
-            AssetsService::BalancesChange(
-                $orderNo,
+            AssetsService::BalancesChange2(
                 $provinceAgentUid,
-                $assets,
+                $assets->id,
                 $assets->assets_name,
                 $provinceAmount,
                 AssetsLogs::OPERATE_TYPE_PROVINCE_REBATE,
-                $remark
+                $remark,
+                $orderNo,
             );
         }
 
@@ -111,14 +86,14 @@ class ProvinceCityAreaDlService
             $remark = '市节点运营返佣';
 //市长分配比列1.25%
             $cityAmount = bcmul($order->profit_price, bcdiv($cityNodeRebate, 100, 6), 8);
-            AssetsService::BalancesChange(
-                $orderNo,
+            AssetsService::BalancesChange2(
                 $cityAgentUid,
-                $assets,
+                $assets->id,
                 $assets->assets_name,
                 $cityAmount,
                 AssetsLogs::OPERATE_TYPE_CITY_REBATE,
-                $remark
+                $remark,
+                $orderNo,
             );
         }
 //区节点返佣
@@ -128,14 +103,14 @@ class ProvinceCityAreaDlService
             $remark = '区级节点运营返佣';
             //区长分配1.75%
             $districtAmount = bcmul($order->profit_price, bcdiv($districtNodeRebate, 100, 6), 8);
-            AssetsService::BalancesChange(
-                $orderNo,
+            AssetsService::BalancesChange2(
                 $districtAgentUid,
-                $assets,
+                $assets->id,
                 $assets->assets_name,
                 $districtAmount,
                 AssetsLogs::OPERATE_TYPE_DISTRICT_REBATE,
-                $remark
+                $remark,
+                $orderNo,
             );
         }
     }

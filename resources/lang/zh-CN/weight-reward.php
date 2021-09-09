c36722a847e434c89286be7f6ba02f6f281094e8
laydown <947714443@qq.com>
2021-09-09 17:16:51 +0800

    weight-reward-log

---
+++ resources/lang/zh-CN/weight-reward.php
@@ -0,0 +1,15 @@
+<?php 
+return [
+    'labels' => [
+        'WeightReward' => 'WeightReward',
+    ],
+    'fields' => [
+        'count_date' => '统计日期',
+        'silver_money' => '银卡已累计金额',
+        'gold_money' => '金卡已累计金额',
+        'diamond_money' => '钻石卡已累计金额',
+        'is_deal' => '是否已处理[分红]',
+    ],
+    'options' => [
+    ],
+];
---
+++ resources/lang/zh-CN/weight-rewards-log.php
@@ -0,0 +1,19 @@
+<?php 
+return [
+    'labels' => [
+        'WeightRewardsLog' => 'WeightRewardsLog',
+    ],
+    'fields' => [
+        'order_no' => '订单号',
+        'count_date' => '统计日期',
+        'silver_money' => '银卡金额',
+        'gold_money' => '金卡金额',
+        'diamond_money' => '钻石计金额',
+        'silver_ratio' => '银卡比例',
+        'gold_ratio' => '金卡比例',
+        'diamond_ratio' => '钻石卡比例',
+        'money' => '订单金额',
+    ],
+    'options' => [
+    ],
+];
